0b6f5eca7bacb6863476e402b4c0145d0a0a6079
Сулейман Гудешев <henexy@mail.ru>
2022-07-10 14:16:31 +0300

    return data

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="todo">
+    <TaskInput @onAddTask="addTask" />
+    <TaskList @OnDone="setDoneTask" @onRemove="setRemoveTask" :models="list" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TaskList from './components/TaskList.vue';
+import TaskInput from './components/TaskInput.vue';
 
+import { ref } from 'vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  components: { TaskList, TaskInput },
+  setup() {
+
+    let list = ref(
+      [
+        {
+          id: 0,
+          title: 'create a new title',
+          description: 'create a new description',
+          done: false
+        }
+      ]
+    );
+
+    let addTask = ({ title, description }) => {
+      list.value.push({
+        id: list.value[list.value.length - 1].id + 1,
+        title: title,
+        description: description,
+        done: false
+      })
+    }
+
+    let setDoneTask = (id) => {
+      list.value.map(task => {
+        if(task.id === id) {
+          task.done = true;
+        }
+        
+        return task;
+      })
+    }
+
+    let setRemoveTask = (id) => {
+      list.value = list.value.filter(task => task.id !== id)
+    }
+
+    return {
+      addTask, list, setDoneTask, setRemoveTask
+    }
   }
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
